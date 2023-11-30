4a959895d0de446ebb482ee1b44c5833fb328e20
Роман Кулагин <romankulagin@Romans-MacBook-Pro.local>
2023-12-01 02:41:48 +0400

    add weather icon

--- src/components/weatherBlock/index.tsx
+++ src/components/weatherBlock/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 
 import { useSelector } from 'react-redux';
 
@@ -14,6 +14,25 @@ import TemperatureIcon from '../../assets/icons/temperature.png';
 import SunRain from '../../assets/images/weather/rain.png';
 import PressureIcon from '../../assets/icons/pressure.png';
 import WindIcon from '../../assets/icons/wind.png';
+
+import icon01d from '../../assets/icons/weatherPictures/01d.png';
+import icon02d from '../../assets/icons/weatherPictures/02d.png';
+import icon03d from '../../assets/icons/weatherPictures/03d.png';
+import icon04d from '../../assets/icons/weatherPictures/04d.png';
+import icon09d from '../../assets/icons/weatherPictures/09d.png';
+import icon10d from '../../assets/icons/weatherPictures/10d.png';
+import icon11d from '../../assets/icons/weatherPictures/11d.png';
+import icon13d from '../../assets/icons/weatherPictures/13d.png';
+import icon50d from '../../assets/icons/weatherPictures/50d.png';
+import icon01n from '../../assets/icons/weatherPictures/01n.png';
+import icon02n from '../../assets/icons/weatherPictures/02n.png';
+import icon03n from '../../assets/icons/weatherPictures/03n.png';
+import icon04n from '../../assets/icons/weatherPictures/04n.png';
+import icon09n from '../../assets/icons/weatherPictures/09n.png';
+import icon10n from '../../assets/icons/weatherPictures/10n.png';
+import icon11n from '../../assets/icons/weatherPictures/11n.png';
+import icon13n from '../../assets/icons/weatherPictures/13n.png';
+import icon50n from '../../assets/icons/weatherPictures/50n.png';
 
 import styles from './weatherBlock.module.scss';
 
@@ -39,6 +58,231 @@ const WeatherBlock: FC<I_Carousel> = ({
 	const { windSpeed, feelsLike, pressure, status, data, temp, wind } =
 		useSelector((state: any) => state.weatherToday.weatherToday);
 
+	const [icon, setIcon] = useState(icon01d);
+	const [miniIcon, setMiniIcon] = useState(icon01d);
+
+	console.log('weatherMini', weatherMini);
+
+	useEffect(() => {
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '01d') {
+				setIcon(icon01d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '02d') {
+				setIcon(icon02d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '03d') {
+				setIcon(icon03d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '04d') {
+				setIcon(icon04d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '09d') {
+				setIcon(icon09d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '10d') {
+				setIcon(icon10d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '11d') {
+				setIcon(icon11d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '13d') {
+				setIcon(icon13d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '50d') {
+				setIcon(icon50d);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '01n') {
+				setIcon(icon01n);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '02n') {
+				setIcon(icon02n);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '03n') {
+				setIcon(icon03n);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '04n') {
+				setIcon(icon04n);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '09n') {
+				setIcon(icon09n);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '10n') {
+				setIcon(icon10n);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '11n') {
+				setIcon(icon11n);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '13n') {
+				setIcon(icon13n);
+			}
+		}
+
+		if (data.weather && data.weather[0]) {
+			if (data.weather[0].icon === '50n') {
+				setIcon(icon50n);
+			}
+		}
+	}, [data]);
+
+	useEffect(() => {
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '01d') {
+				setMiniIcon(icon01d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '02d') {
+				setMiniIcon(icon02d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '03d') {
+				setMiniIcon(icon03d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '04d') {
+				setMiniIcon(icon04d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '09d') {
+				setMiniIcon(icon09d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '10d') {
+				setMiniIcon(icon10d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '11d') {
+				setMiniIcon(icon11d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '13d') {
+				setMiniIcon(icon13d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '50d') {
+				setMiniIcon(icon50d);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '01n') {
+				setMiniIcon(icon01n);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '02n') {
+				setMiniIcon(icon02n);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '03n') {
+				setMiniIcon(icon03n);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '04n') {
+				setMiniIcon(icon04n);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '09n') {
+				setMiniIcon(icon09n);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '10n') {
+				setMiniIcon(icon10n);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '11n') {
+				setMiniIcon(icon11n);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '13n') {
+				setMiniIcon(icon13n);
+			}
+		}
+
+		if (weatherMini && weatherMini[0]) {
+			if (weatherMini[0].icon === '50n') {
+				setMiniIcon(icon50n);
+			}
+		}
+	}, [weatherMini]);
+
 	return (
 		<div
 			ref={ref}
@@ -49,11 +293,15 @@ const WeatherBlock: FC<I_Carousel> = ({
 			)}
 		>
 			{isToday ? (
-				<h2 className={styles.height48}>{data.dt && formatDateTimestamp(data.dt)}</h2>
+				<h2 className={styles.height48}>
+					{data.dt && formatDateTimestamp(data.dt)}
+				</h2>
 			) : isCarousel ? (
 				<h2 className={styles.height48}>{formatDateTimestamp(timeMini)}</h2>
 			) : (
-				<h2 className={styles.height48}>{data.dt && formatDateTimestamp(data.dt)}</h2>
+				<h2 className={styles.height48}>
+					{data.dt && formatDateTimestamp(data.dt)}
+				</h2>
 			)}
 			{status === 'loaded' ? (
 				<>
@@ -68,7 +316,7 @@ const WeatherBlock: FC<I_Carousel> = ({
 			) : (
 				<p className={styles.height}></p>
 			)}
-			<img className={styles.weatherPick} src={SunRain} alt='weather' />
+			<img className={styles.weatherPick} src={isCarousel && isToday ? icon : isCarousel ? miniIcon : icon} alt='weather' />
 			{isToday ? (
 				<div className={styles.metricsInCarousel}>
 					<p>Температура {temp && temp} С</p>
